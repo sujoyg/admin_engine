16cde44dfcf57fb3ceda0fa68e818f39471a63ab
Sujoy Gupta <sujoy@pingbooth.com>
2012-10-30 14:47:06 -0700

    Require the user model in the after initializer block.

--- lib/admin_engine.rb
+++ lib/admin_engine.rb
@@ -5,14 +5,9 @@ module AdminEngine
   class Engine < Rails::Engine
     engine_name 'admin_engine'
 
-    #initializer 'admin_engine.app_controller' do |app|
-    #  ActiveSupport.on_load(:action_controller) do
-    #    require File.expand_path('../../app/controllers/application_controller', __FILE__)
-    #  end
-    #end
-
     config.after_initialize do
       require File.expand_path('../../app/controllers/application_controller', __FILE__)
+      require File.expand_path('../../app/models/user', __FILE__)
     end
   end
 end
